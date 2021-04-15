fa511ec106d238b8aecf100de658899d2f9cd182
Will <willblake13@gmail.com>
2021-04-15 11:23:08 -0400

    fixed github routing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contact from './contents/Contact';
 
   function App() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <div className="App">
           <Navbar/>
@@ -34,7 +34,7 @@ import Contact from './contents/Contact';
           </Route>
           </div>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
   export default App;
